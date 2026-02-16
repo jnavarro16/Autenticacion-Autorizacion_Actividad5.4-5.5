f4157b15bfbde0dbe4879fea963125741b5395d6
«Jordi <jornavmar6@alu.edu.gva.es>
2026-02-16 20:26:21 +0100

    Paquete Seguridad - Clase: WebSecurityConfig (Modificada)

--- src/main/java/agenda/Seguridad/WebSecurityConfig.java
+++ src/main/java/agenda/Seguridad/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -19,9 +20,10 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers(HttpMethod.OPTIONS, Constans.LOGIN_URL).permitAll()
+                        .requestMatchers(HttpMethod.POST, Constans.LOGIN_URL).permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/contactos/**").hasAuthority("ROLE_ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterAfter(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
